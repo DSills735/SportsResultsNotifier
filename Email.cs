8d263ecc030ef68c22b3dc16bea4ca637156101b
DSills735 <David.sills100@gmail.com>
2026-05-08 09:46:20 -0500

    added test email logic

---
+++ Email.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mail;
+
+namespace SportsResultsNotifier;
+
+internal class Email
+{
+        
+    public static void Send(string subject, string body)
+    {
+       string smtpAddress = "smtp.gmail.com";
+        int portNumber = 587;
+        bool enableSSL = true;
+        string fromEmail = "david.sills100@gmail.com";
+        string toEmail = "recipient@example.com";
+        string password = "your_email_password";
+        string subject = "test";
+        string body = "This is a test email from C# application.";
+
+        using (MailMessage mail = new MailMessage())
+        {
+            mail.From = new MailAddress(fromEmail);
+            mail.To.Add(toEmail);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = false;
+            using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+            {
+                smtp.Credentials = new System.Net.NetworkCredential(fromEmail, password);
+                smtp.EnableSsl = enableSSL;
+                smtp.Send(mail);
+            }
+        }
+    }
+}
