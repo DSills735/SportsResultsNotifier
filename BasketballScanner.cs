7de73d268a0e019d1f1792219d6899291098d0a9
DSills735 <David.sills100@gmail.com>
2026-05-08 09:32:55 -0500

    games are returnign as they should.

--- BasketballScanner.cs
+++ BasketballScanner.cs
@@ -8,27 +8,32 @@ internal class BasketballScanner
     public static void Scan()
     {
         HtmlWeb web = new HtmlWeb();
-        //HtmlDocument document = web.Load("https://www.basketball-reference.com/boxscores/");
-        HtmlDocument document = web.Load("https://www.basketball-reference.com/boxscores/?month=05&day=01&year=2026");
+        HtmlDocument document = web.Load("https://www.basketball-reference.com/boxscores/");
 
-        var games = document.DocumentNode.SelectNodes("//div[@class='game_summary expanded nohover']");
+
+
+
+        var games = document.DocumentNode.SelectNodes("//div[contains(@class, 'game_summary')]");
 
         if (games != null)
         {
             foreach (var game in games)
             {
-                var winner = game.SelectSingleNode(".//tr[@class='winner']").InnerText.Trim();
-                var loser = game.SelectSingleNode(".//tr[@class='loser']").InnerText.Trim();
+                var teams = game.SelectNodes(".//tr");
 
-                Console.WriteLine($"Winner: {winner}, Loser: {loser}");
+                if (teams != null && teams.Count >= 2)
+                {
+                    string team1 = teams[0].InnerText.Trim();
+                    string team2 = teams[1].InnerText.Trim();
+
+                    Console.WriteLine($"Game Found: {team1} vs {team2}");
+                }
             }
         }
         else
         {
-            Console.WriteLine("No games played today.");
+            Console.WriteLine("No games found on the page.");
         }
+
     }
 }
-
-
-///html/body/div[4]/div[3]/div[2]/div[1]/p/strong <- leads to no games played today, but if there were games, the path would be: //div[@class='game_summary expanded nohover']
