7542dc2e95dfbe349d06f07c43d38d4e1da9df4a
David Sills <david.sills100@gmail.com>
2026-05-11 16:23:07 -0500

    added logic to only only scan every day at 8am, otherwise check the time every 10 seconds.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 ﻿using HtmlAgilityPack;
 
 namespace SportsResultsNotifier;
-
 public class Program
 {
     public static void Main(string[] args)
     {
-        BasketballScanner.Scan();
+        TimeOnly runningTime = new TimeOnly(8, 0, 0);
+        Console.WriteLine($"Waiting until {runningTime} to scan...");
+
+        while (true)
+        {
+            if (TimeOnly.FromDateTime(DateTime.Now) >= runningTime)
+            {
+                BasketballScanner.Scan();
+                break; 
+            }
+            Thread.Sleep(10000);
+        }
     }
+
 }
